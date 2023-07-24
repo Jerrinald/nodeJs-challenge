d495fd18ba5083bc083814d6036efa692ca4f8ca
Ricardogn224 <abrahamricardogn224@gmail.com>
2023-07-24 16:30:56 +0200

    set up ecom api

---
+++ project/ecom-api/services/marchand.js
@@ -0,0 +1,75 @@
+const { Marchand } = require("../db");
+const Sequelize = require("sequelize");
+const ValidationError = require("../errors/ValidationError");
+
+module.exports = function MarchandService() {
+    return {
+        findAll: async function (filters, options) {
+            let dbOptions = {
+                where: filters,
+            };
+            if (options.order) {
+                dbOptions.order = Object.entries(options.order);
+            }
+            if (options.limit) {
+                dbOptions.limit = options.limit;
+                dbOptions.offset = options.offset;
+            }
+            return Marchand.findAll(dbOptions);
+        },
+        findOne: async function (filters) {
+
+            return Marchand.findOne({ where: filters });
+        },
+        create: async function (data) {
+            try {
+                return await Marchand.create(data);
+            } catch (e) {
+                if (e instanceof Sequelize.ValidationError) {
+                    throw ValidationError.fromSequelizeValidationError(e);
+                }
+                throw e;
+            }
+        },
+        replace: async function (filters, newData) {
+            try {
+                if (!filters || Object.keys(filters).length === 0) {
+                    throw new Error("Invalid filters.");
+                }
+                const nbDeleted = await this.delete(filters);
+                const marchand = await this.create(newData);
+                return [[marchand, nbDeleted === 0]];
+            } catch (e) {
+                if (e instanceof Sequelize.ValidationError) {
+                    throw ValidationError.fromSequelizeValidationError(e);
+                }
+                throw e;
+            }
+        },
+        update: async function (filters, newData) {
+            try {
+                if (!filters || Object.keys(filters).length === 0) {
+                    throw new Error("Invalid filters.");
+                }
+                const [nbUpdated, marchands] = await Marchand.update(newData, {
+                    where: filters,
+                    returning: true,
+                    individualHooks: true,
+                });
+
+                return marchands;
+            } catch (e) {
+                if (e instanceof Sequelize.ValidationError) {
+                    throw ValidationError.fromSequelizeValidationError(e);
+                }
+                throw e;
+            }
+        },
+        delete: async function (filters) {
+            if (!filters || Object.keys(filters).length === 0) {
+                throw new Error("Invalid filters.");
+            }
+            return Marchand.destroy({ where: filters });
+        },
+    };
+};
---
+++ project/ecom-api/services/product.js
@@ -0,0 +1,77 @@
+const { Product } = require("../db");
+const Sequelize = require("sequelize");
+const ValidationError = require("../errors/ValidationError");
+
+module.exports = function ProductService() {
+    return {
+        findAll: async function (filters, options) {
+            let dbOptions = {
+                where: filters,
+            };
+            if (options.order) {
+                dbOptions.order = Object.entries(options.order);
+            }
+            if (options.limit) {
+                dbOptions.limit = options.limit;
+                dbOptions.offset = options.offset;
+            }
+            return Product.findAll(dbOptions);
+        },
+        findOne: async function (filters) {
+
+            return Product.findOne({ where: filters });
+        },
+        create: async function (data) {
+            try {
+                data.marchandId = data.currentUserId;
+                delete data['currentUserId'];
+                return await Product.create(data);
+            } catch (e) {
+                if (e instanceof Sequelize.ValidationError) {
+                    throw ValidationError.fromSequelizeValidationError(e);
+                }
+                throw e;
+            }
+        },
+        replace: async function (filters, newData) {
+            try {
+                if (!filters || Object.keys(filters).length === 0) {
+                    throw new Error("Invalid filters.");
+                }
+                const nbDeleted = await this.delete(filters);
+                const Product = await this.create(newData);
+                return [[Product, nbDeleted === 0]];
+            } catch (e) {
+                if (e instanceof Sequelize.ValidationError) {
+                    throw ValidationError.fromSequelizeValidationError(e);
+                }
+                throw e;
+            }
+        },
+        update: async function (filters, newData) {
+            try {
+                if (!filters || Object.keys(filters).length === 0) {
+                    throw new Error("Invalid filters.");
+                }
+                const [nbUpdated, products] = await Product.update(newData, {
+                    where: filters,
+                    returning: true,
+                    individualHooks: true,
+                });
+
+                return products;
+            } catch (e) {
+                if (e instanceof Sequelize.ValidationError) {
+                    throw ValidationError.fromSequelizeValidationError(e);
+                }
+                throw e;
+            }
+        },
+        delete: async function (filters) {
+            if (!filters || Object.keys(filters).length === 0) {
+                throw new Error("Invalid filters.");
+            }
+            return Product.destroy({ where: filters });
+        },
+    };
+};
