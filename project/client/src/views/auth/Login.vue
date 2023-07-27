6a509fb5f07a1e2c2f07ad07165792eae19a88cd
Jerrinald <jerrinald95190@live.fr>
2023-07-27 21:30:49 +0200

    login two forms

--- project/client/src/views/auth/Login.vue
+++ project/client/src/views/auth/Login.vue
@@ -8,11 +8,16 @@
     <div class="backdrop" @click.self="toggleModal"></div>
     <div class="modal-box">
       <div class="modal-title">
-        Connectez-vous<slot name="close-icon" :closeModal="toggleModal"></slot>
+        Connexion<slot name="close-icon" :closeModal="toggleModal"></slot>
       </div>
       <div class="modal-content">
-        <div class="login-form">
+        <div class="form-selection">
+          <button @click="showMarchandForm" class="form-selection-btn marchand" :class="{ active: selectedForm === 'marchand' }">Marchand</button>
+          <button @click="showSimpleUserForm" class="form-selection-btn simple-user" :class="{ active: selectedForm === 'simpleUser' }">Simple User</button>
+        </div>
+        <div class="login-form" v-if="selectedForm === 'marchand'">
           <form @submit.prevent="loginUser" class="flex fdc gap-15">
+            <h2>Connectez vous en tant que marchand</h2>
             <div>
               <div>
                 <label for="email">Email:</label>
@@ -27,6 +32,23 @@
             </div>
             <div class="flex jce">
               <button class="btn-primary" type="submit">Connecter</button>
+            </div>
+          </form>
+        </div>
+
+        <div class="login-form" v-else>
+          <form @submit.prevent="loginUserSimple" class="flex fdc gap-25">
+            <h2>Connectez vous en tant qu'utilisateur</h2>
+            <div>
+              <label for="email">Email:</label>
+              <input type="email" id="email" v-model="userSimple.email" required>
+            </div>
+            <div>
+              <label for="password">Password:</label>
+              <input type="password" id="password" v-model="userSimple.password" required>
+            </div>
+            <div class="flex jce">
+              <button type="submit" class="btn btn-primary">Connexion</button>
             </div>
           </form>
         </div>
@@ -53,10 +75,25 @@ let user = reactive({
   password: ''
 });
 
+let userSimple = reactive({
+  email: '',
+  password: ''
+});
+
 const openModal = ref(false);
 
 function toggleModal() {
   openModal.value = !openModal.value;
+}
+
+let selectedForm = ref('marchand'); // Default to displaying the "Marchand" form
+
+function showMarchandForm() {
+  selectedForm.value = 'marchand';
+}
+
+function showSimpleUserForm() {
+  selectedForm.value = 'simpleUser';
 }
 
 async function loginUser() {
@@ -67,6 +104,33 @@ async function loginUser() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(user)
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      // Gérer la réponse de l'API en fonction de vos besoins
+      console.log(data);
+      const tokenExpiration = Date.now() + 3600 * 1000; // La durée doit correspondre à celle que vous avez définie côté serveur (ici 1 heure)
+
+      // on rajoute le token dans le localStorage
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('tokenExpiration', tokenExpiration);
+    } else {
+      console.error('Login failed');
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
+async function loginUserSimple() {
+  try {
+    const response = await fetch('http://localhost:3000/loginUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userSimple)
     });
 
     if (response.ok) {
@@ -143,4 +207,34 @@ async function loginUser() {
   padding: 20px;
   border: 1px solid #ccc;
   border-radius: 5px;
-}</style>
+}
+
+
+.form-selection {
+  display: flex;
+  gap: 10px;
+}
+
+.form-selection-btn {
+  padding: 10px 20px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  background-color: #f2f2f2;
+  color: #333;
+  cursor: pointer;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.form-selection-btn.active {
+  background-color: #50cea3;
+  color: #fff;
+}
+
+.form-selection-btn.marchand {
+  border-radius: 5px 0 0 5px;
+}
+
+.form-selection-btn.simple-user {
+  border-radius: 0 5px 5px 0;
+}
+</style>
