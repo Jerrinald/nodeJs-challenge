a6e380979bbbfa60f598d5eaba214fe640d4393b
Nfassory Diaby <fassory.diaby@gmail.com>
2023-07-28 23:22:44 +0200

    tests unitaires

---
+++ project/ecom-api/test/product.test.js
@@ -0,0 +1,76 @@
+const ProductService = require("../services/product.js");
+const { Product } = require("../db");
+const Sequelize = require("sequelize");
+
+// Mock Sequelize model methods
+jest.mock("../db", () => ({
+    Product: {
+        findAll: jest.fn(),
+        findOne: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+        destroy: jest.fn(),
+    }
+}));
+
+describe("ProductService", () => {
+    let service;
+
+    beforeEach(() => {
+        service = ProductService();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("should return all products", async () => {
+        const mockProducts = [{ id: 1, name: "Product 1" }, { id: 2, name: "Product 2" }];
+        Product.findAll.mockResolvedValue(mockProducts);
+        const products = await service.findAll({});
+        expect(products).toEqual(mockProducts);
+        expect(Product.findAll).toHaveBeenCalledTimes(1);
+    });
+
+    it("should find one product", async () => {
+        const mockProduct = { id: 1, name: "Product 1" };
+        Product.findOne.mockResolvedValue(mockProduct);
+        const product = await service.findOne({ id: 1 });
+        expect(product).toEqual(mockProduct);
+        expect(Product.findOne).toHaveBeenCalledTimes(1);
+    });
+
+    it("should create a product", async () => {
+        const mockProduct = { id: 1, name: "Product 1" };
+        Product.create.mockResolvedValue(mockProduct);
+        const product = await service.create({ name: "Product 1" });
+        expect(product).toEqual(mockProduct);
+        expect(Product.create).toHaveBeenCalledTimes(1);
+    });
+
+    it("should replace a product", async () => {
+        const mockProduct = { id: 1, name: "Product 1" };
+        Product.destroy.mockResolvedValue(1);
+        Product.create.mockResolvedValue(mockProduct);
+        const [product, wasReplaced] = await service.replace({ id: 1 }, { name: "Product 1" });
+        expect(product).toEqual(mockProduct);
+        expect(wasReplaced).toEqual(false);
+        expect(Product.destroy).toHaveBeenCalledTimes(1);
+        expect(Product.create).toHaveBeenCalledTimes(1);
+    });
+
+    it("should update a product", async () => {
+        const mockProduct = { id: 1, name: "Product 1" };
+        Product.update.mockResolvedValue([1, [mockProduct]]);
+        const products = await service.update({ id: 1 }, { name: "Product 1" });
+        expect(products).toEqual([mockProduct]);
+        expect(Product.update).toHaveBeenCalledTimes(1);
+    });
+
+    it("should delete a product", async () => {
+        Product.destroy.mockResolvedValue(1);
+        const nbDeleted = await service.delete({ id: 1 });
+        expect(nbDeleted).toEqual(1);
+        expect(Product.destroy).toHaveBeenCalledTimes(1);
+    });
+});
