bb4e15e9dfa74f419921399642efc728b61b106d
Karim BETKOM <contact@karim-betkom.com>
2023-08-06 17:15:38 +0200

    update client page

--- project/client/src/views/auth/Login.vue
+++ project/client/src/views/auth/Login.vue
@@ -11,13 +11,9 @@
         Connexion<slot name="close-icon" :closeModal="toggleModal"></slot>
       </div>
       <div class="modal-content">
-        <div class="form-selection flex jcc">
-          <button @click="showMarchandForm" class="form-selection-btn marchand" :class="{ active: selectedForm === 'marchand' }">Marchand</button>
-          <button @click="showSimpleUserForm" class="form-selection-btn simple-user" :class="{ active: selectedForm === 'simpleUser' }">Simple User</button>
-        </div>
-        <div class="login-form" v-if="selectedForm === 'marchand'">
+        <div class="login-form">
           <form @submit.prevent="loginUser" class="flex fdc gap-15">
-            <h2>Connectez vous en tant que marchand</h2>
+            <h2>Connectez vous</h2>
             <div>
               <div>
                 <label for="email">Email:</label>
@@ -32,23 +28,6 @@
             </div>
             <div class="flex jce">
               <button class="btn-primary" type="submit">Connecter</button>
-            </div>
-          </form>
-        </div>
-
-        <div class="login-form" v-else>
-          <form @submit.prevent="loginUserSimple" class="flex fdc gap-25">
-            <h2>Connectez vous en tant qu'utilisateur</h2>
-            <div>
-              <label for="email">Email:</label>
-              <input type="email" id="email" v-model="userSimple.email" required>
-            </div>
-            <div>
-              <label for="password">Password:</label>
-              <input type="password" id="password" v-model="userSimple.password" required>
-            </div>
-            <div class="flex jce">
-              <button type="submit" class="btn btn-primary">Connexion</button>
             </div>
           </form>
         </div>
@@ -76,28 +55,15 @@ let user = reactive({
   password: ''
 });
 
-let userSimple = reactive({
-  email: '',
-  password: ''
-});
-
 const openModal = ref(false);
 
 function toggleModal() {
   openModal.value = !openModal.value;
 }
 
-let selectedForm = ref('marchand'); // Default to displaying the "Marchand" form
-
-function showMarchandForm() {
-  selectedForm.value = 'marchand';
-}
-
-function showSimpleUserForm() {
-  selectedForm.value = 'simpleUser';
-}
-
 async function loginUser() {
+
+  console.log(user)
   try {
     const response = await fetch(`${import.meta.env.VITE_API_PAIEMENT}/login`, {
       method: 'POST',
@@ -121,38 +87,6 @@ async function loginUser() {
       store.dispatch('login', data.marchand);
 
       router.push('/profile'); 
-    } else {
-      console.error('Login failed');
-    }
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-}
-
-async function loginUserSimple() {
-  try {
-    const response = await fetch('http://localhost:3000/loginUser', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(userSimple)
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      // Gérer la réponse de l'API en fonction de vos besoins
-      console.log(data);
-      const tokenExpiration = Date.now() + 3600 * 1000; // La durée doit correspondre à celle que vous avez définie côté serveur (ici 1 heure)
-
-      // on rajoute le token dans le localStorage
-      localStorage.setItem('token', data.token);
-      localStorage.setItem('tokenExpiration', tokenExpiration);
-
-      // on redirige vers la page d'accueil
-      store.dispatch('login', data.user);
-
-      router.push('/'); 
     } else {
       console.error('Login failed');
     }
@@ -234,11 +168,6 @@ async function loginUserSimple() {
   color: #333;
   cursor: pointer;
   transition: background-color 0.3s, color 0.3s;
-}
-
-.form-selection-btn.active {
-  background-color: #50cea3;
-  color: #fff;
 }
 
 .form-selection-btn.marchand {
